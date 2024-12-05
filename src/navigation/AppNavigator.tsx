b8e4c1314eafa783a0dc72c5d2f496d9c93e032c
charlie <charles-andre.arsenec@epita.fr>
2024-12-05 16:10:10 +0100

    loading typo in LoadingScreen

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -3,28 +3,35 @@ import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
 
 // Import screens
-import { LoadingScreen } from '../screens/LoadingScreen/LoadingScreen'; 
-import { HomeScreen } from '../screens/HomeScreen/HomeScreen'; 
+import { LoadingScreen } from '../screens/LoadingScreen/LoadingScreen';
+import { HomeScreen } from '../screens/HomeScreen/HomeScreen';
+import TypographyExampleScreen from '../screens/CoreExampleScreen/TypographyExampleScreen';
 
 const Stack = createStackNavigator();
 
 const AppNavigator = () => {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="LaunchScreen">
-        <Stack.Screen
-          name="LaunchScreen"
-          component={LoadingScreen}
-          options={{ headerShown: false }} 
-        />
-        <Stack.Screen
-          name="HomeScreen"
-          component={HomeScreen}
-          options={{ headerShown: true, title: 'Home' }} 
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+    return (
+        <NavigationContainer>
+            <Stack.Navigator initialRouteName="LaunchScreen">
+                <Stack.Screen
+                    name="LaunchScreen"
+                    component={LoadingScreen}
+                    options={{ headerShown: false }}
+                />
+                <Stack.Screen
+                    name="HomeScreen"
+                    component={HomeScreen}
+                    options={{ headerShown: true, title: 'Home' }}
+                />
+
+                <Stack.Screen
+                    name="Typo"
+                    component={TypographyExampleScreen}
+                    options={{ headerShown: true, title: 'Typo' }}
+                />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
 };
 
 export default AppNavigator;
--- src/screens/LoadingScreen/LoadingScreen.tsx
+++ src/screens/LoadingScreen/LoadingScreen.tsx
@@ -1,33 +1,55 @@
 import React, { useEffect } from 'react';
 import { View, StyleSheet, Image, Text } from 'react-native';
 import { Images, Animations } from '../../assets/index';
+import { useFonts } from 'expo-font';
 import LottieView from 'lottie-react-native';
 
 import styles from './LoadingScreen.styles';
 
-export function LoadingScreen({navigation} : any){
-  useEffect(() => {
-    // Simulate loading andimport styles from './LoadingScreen.styles'; navigate to Home after 3 seconds
-    const timer = setTimeout(() => {
-      // Navigate to Home and remove LaunchScreen from the stack
-      navigation.replace('HomeScreen'); 
-    }, 1300);
+export function LoadingScreen({ navigation }: any) {
+    const font_base_bath = '../../assets/fonts/';
 
-    return () => clearTimeout(timer); // Clear the timer on unmount
-  }, [navigation]);
+    const [fontsLoaded] = useFonts({
+        'DM-Sans-Regular': require(
+            `${font_base_bath}DM-Sans/DMSans-Regular.ttf`,
+        ),
+        'DM-Sans-Bold': require(`${font_base_bath}DM-Sans/DMSans-Bold.ttf`),
+        'DM-Sans-Italic': require(`${font_base_bath}DM-Sans/DMSans-Italic.ttf`),
+        'Poppins-Regular': require(
+            `${font_base_bath}Poppins/Poppins-Regular.ttf`,
+        ),
+        'Poppins-SemiBold': require(
+            `${font_base_bath}Poppins/Poppins-SemiBold.ttf`,
+        ),
+        'Poppins-Bold': require(`${font_base_bath}Poppins/Poppins-Bold.ttf`),
+        'Poppins-Italic': require(
+            `${font_base_bath}Poppins/Poppins-Italic.ttf`,
+        ),
+    });
 
+    useEffect(() => {
+        let timer: NodeJS.Timeout;
 
-  return (
-    <View style={styles.loading_screen}>
-      <View style={styles.container}>
-        <Image source={Images.logo_wearit} style={styles.logo} />
-      </View>
-      <LottieView
-          source={Animations.loading_spinner}
-          autoPlay
-          loop
-          style={styles.animation}
-      />
-    </View>
-  );
-};
+        if (!fontsLoaded) {
+            timer = setTimeout(() => {
+                navigation.replace('Typo');
+            }, 1300);
+        }
+
+        return () => clearTimeout(timer);
+    }, [navigation]);
+
+    return (
+        <View style={styles.loading_screen}>
+            <View style={styles.container}>
+                <Image source={Images.logo_wearit} style={styles.logo} />
+            </View>
+            <LottieView
+                source={Animations.loading_spinner}
+                autoPlay
+                loop
+                style={styles.animation}
+            />
+        </View>
+    );
+}
