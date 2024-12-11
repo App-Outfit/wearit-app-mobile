4bab25c069e74b0d52c0ec2de8b705cfa8216ecc
charlie <charles-andre.arsenec@epita.fr>
2024-12-12 00:31:58 +0100

    text fields

--- src/assets/index.tsx
+++ src/assets/index.tsx
@@ -1,3 +1,6 @@
+import { Icon } from 'react-native-elements';
+import { lightTheme } from '../styles/theme';
+
 type ImageSource = number; // React Native images are required as numbers
 type AnimationSource = string; // Lottie animations are usually JSON objects
 
@@ -8,6 +11,23 @@ export const Images: Record<string, ImageSource> = {
 
 // Icons
 export const Icons: Record<string, ImageSource> = {};
+export const circleCheckIcon = (
+    <Icon
+        name="checkmark-circle-outline"
+        type="ionicon"
+        iconProps={{ size: 24, name: 'checkmark-circle-outline' }}
+        color={lightTheme.colors.primary}
+    />
+);
+export const circleErrorIcon = (
+    <Icon
+        name="exclamationcircleo"
+        type="antdesign"
+        iconProps={{ size: 20, name: 'exclamationcircleo' }}
+        color={lightTheme.colors.error}
+        style={{ marginRight: 2 }}
+    />
+);
 
 // Animations
 export const Animations: Record<string, AnimationSource> = {
