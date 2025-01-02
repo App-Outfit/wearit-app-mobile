b046a5fde303fb685509df2ad10d8163bbbe85c0
Bast-94 <bh.69@live.fr>
2025-01-02 18:09:33 +0100

    DOC(signUpService).

--- src/services/signupService.tsx
+++ src/services/signupService.tsx
@@ -1,7 +1,7 @@
 // TODO Implement a new function which will add a new registered user to the Mongo database
 // create an axios post request to the backend to add a new user to the databaseimport axios from 'axios';
 import axios from 'axios';
-const BASE_URL = 'http://192.168.1.18:8000';
+const BASE_URL = 'http://192.168.1.18:8000'; // TODO: Change this to the server's IP address because it hardcoded to the local IP address
 const AUTH_URL = `${BASE_URL}/auth/signup`;
 const authAPI = axios.create({
     baseURL: BASE_URL,
@@ -17,7 +17,7 @@ async function signUpUser(
             console.error('Missing email, password or name');
             return null;
         }
-        console.log('Signing up user:', email);
+
         const response = await authAPI.post('api/auth/signup', {
             email: email,
             password: password,
@@ -29,20 +29,26 @@ async function signUpUser(
         }
         return response;
     } catch (error) {
-        if (error.response) {
-            if (error.response.status === 400) {
-                console.error('Bad request', error.response.data);
-            } else if (error.response.status === 401) {
-                console.error('Unauthorized');
-            } else if (error.response.status === 403) {
+        let objError = error as any; // Avoiding typescript error
+        if (objError.response) {
+            if (objError.response.status === 400) {
+                if (objError.response.data && objError.response.data.detail) {
+                    console.error(objError.response.data.detail);
+                    return objError.response;
+                } else {
+                    console.error('Bad request', objError.response.data);
+                }
+            } else if (objError.response.status === 401) {
+                console.error('signUpUser: Unauthorized');
+            } else if (objError.response.status === 403) {
                 console.error('Forbidden');
-            } else if (error.response.status === 404) {
+            } else if (objError.response.status === 404) {
                 console.error('Not found');
-            } else if (error.response.status === 500) {
+            } else if (objError.response.status === 500) {
                 console.error('Internal server error');
             }
-        } else if (error.request) {
-            console.error('Error request:', error.request);
+        } else if (objError.request) {
+            console.error('Error request:', objError.request);
         }
         return null;
     }
